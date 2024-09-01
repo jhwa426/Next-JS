ec81b3eb7cdaa43617989fe75802d9bf82f2353d
Jeff Hwang <wodud6359@gmail.com>
2024-09-02 11:53:34 +1200

    Homepage

--- promptopia/app/layout.jsx
+++ promptopia/app/layout.jsx
@@ -9,18 +9,19 @@ export const metadata = {
     description: "Discover & Share AI Prompts",
 };
 
-
 const RootLayout = ({ children }) => {
     return (
         <html lang='en'>
             <body>
-                <div className='main'>
-                    <div className='gradient' />
-                </div>
-                <main className='app'>
-                    <Nav />
-                    {children}
-                </main>
+                <Provider>
+                    <div className='main'>
+                        <div className='gradient' />
+                    </div>
+                    <main className='app'>
+                        <Nav />
+                        {children}
+                    </main>
+                </Provider>
             </body>
         </html>
     );

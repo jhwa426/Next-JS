c305e1436409895045948b106c28add67365bce5
Jeff Hwang <wodud6359@gmail.com>
2024-09-03 13:31:43 +1200

    Homepage

--- promptopia/app/api/auth/[...nextauth]/route.js
+++ promptopia/app/api/auth/[...nextauth]/route.js
@@ -1,12 +1,14 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 
+import { connectToDB } from "@utils/database";
+
 
 const handler = NextAuth({
     providers: [
         GoogleProvider({
-            clientId: "",
-            clientSecret: "",
+            clientId: process.env.GOOGLE_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         })
     ],
 
@@ -15,7 +17,24 @@ const handler = NextAuth({
     },
 
     async sign({ profile }) {
+        try {
+            // serverless -> Lambda -> dynamic
+            await connectToDB();
 
+            // check if user already exists
+
+
+
+            // if not, create a new document and save user in MongoDB
+
+
+
+            return true
+
+        } catch (error) {
+            console.log("Error checking if user exists: ", error.message);
+            return false
+        }
     },
 });
 
